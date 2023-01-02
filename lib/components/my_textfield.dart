622240d0d35bfe21788d82a551cb9c58d61ceddf
OnArap <onarap95@gmail.com>
2023-01-02 12:03:13 +0100

    LoginPage UI

---
+++ lib/components/my_textfield.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class MyTextField extends StatelessWidget {
+  final controller;
+  final String hintText;
+  final bool obscureText;
+
+  const MyTextField({super.key, required this.controller, required this.hintText, required this.obscureText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: TextField(
+          controller: controller,
+          obscureText: obscureText,
+          style: TextStyle(color: Colors.grey.shade300),
+          decoration: InputDecoration(
+              enabledBorder: const OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.black),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey.shade500),
+              ),
+              fillColor: Colors.grey.shade900,
+              filled: true,
+              hintText: hintText,
+              hintStyle: TextStyle(color: Colors.grey.shade600))),
+    );
+  }
+}
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,126 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:melee_notes/components/my_button.dart';
+import 'package:melee_notes/components/my_textfield.dart';
+import 'package:melee_notes/components/square_tile.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  LoginPage({super.key});
+
+  // text editing controllers
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  // sign user in method
+  signUserIn() {}
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[800],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'lib/images/meleenotes_logo.png',
+                height: 130,
+              ),
+              const SizedBox(height: 25),
+              Text(
+                'Welcome back, you\'ve been missed!',
+                style: TextStyle(
+                  color: Colors.grey[300],
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(height: 25),
+              MyTextField(
+                controller: usernameController,
+                hintText: 'email',
+                obscureText: false,
+              ),
+              const SizedBox(height: 10),
+              MyTextField(
+                controller: passwordController,
+                hintText: 'password',
+                obscureText: true,
+              ),
+              const SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Forgot password?',
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 25),
+              MyButton(
+                onTap: signUserIn(),
+              ),
+              const SizedBox(height: 30),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Text(
+                        'Or continue with',
+                        style: TextStyle(color: Colors.grey[400]),
+                      ),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  SquareTile(imagePath: 'lib/images/facebook_logo.png'),
+                  SizedBox(width: 10),
+                  SquareTile(imagePath: 'lib/images/google_logo.png'),
+                ],
+              ),
+              const SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Not a member?',
+                      style: TextStyle(
+                        color: Colors.grey[600],
+                      )),
+                  const SizedBox(width: 4),
+                  const Text('Register now',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      )),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
