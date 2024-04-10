93f0868ccc8c90947c7a28aa14c4778ccf3fc239
OnArap <onarap95@gmail.com>
2024-04-10 17:01:50 +0200

    add gitattributes

--- lib/components/note_tile.dart
+++ lib/components/note_tile.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:melee_notes/constants/colorsFilter.dart';
 
 class NoteTile extends ListTile {
